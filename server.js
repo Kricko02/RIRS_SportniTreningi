21134623600ac92a6dedff2882391fe8303c4d78
domenbencak <domen.bencak@student.um.si>
2025-10-24 12:08:39 +0200

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/postRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // add Authorization here
+    optionsSuccessStatus: 204,
+  })
+);
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Server running ✅");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+const PORT = process.env.PORT || 3242;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
